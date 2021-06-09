64289a95eed4d1ca8cb9cb2a16e5d47ae585bc40
sarthak78381 <sarthakg2k29@gmail.com>
2021-06-09 13:44:58 +0530

    added footer

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const headerContainer = document.getElementById('header-container');
+const headerLogoText = document.getElementById('header-logoText');
 
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY < 100) {
+    if (window.scrollY < window.innerHeight/10) {
         headerContainer.style.color = '#000'
-        return headerContainer.style.background = 'rgba(255,0,0,0)'
-    };
-    headerContainer.style.color = '#fff'
-    return headerContainer.style.background = 'rgba(0,0,0,.7)'
+        headerContainer.style.background = 'rgba(255,0,0,0)'
+        
+    } else {
+        headerContainer.style.color = '#fff'
+        headerContainer.style.background = 'rgba(0,0,0,.7)';
+        
+  
+    }
 })
